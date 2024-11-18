65cb8c13f25339dc44c068b8ca03572ae0959536
nirvana726 <vc77it18@vedascollege.edu.np>
2024-11-18 09:17:49 -0500

    Completed Task 1 - Get book list and Task 2 - Get book details by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,16 +11,25 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+    // Respond with the list of books formatted neatly
+    const formattedBooks = JSON.stringify(books, null, 2); // Neatly formats the JSON output
+    res.status(200).send(formattedBooks);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;  // Retrieve the ISBN from request parameters
+    
+    // Check if the book exists in the books list
+    if (books[isbn]) {
+        // Return the book details as a JSON response
+        res.status(200).json(books[isbn]);
+    } else {
+        // If the book is not found, return a 404 error with a message
+        res.status(404).json({ message: "Book not found" });
+    }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
