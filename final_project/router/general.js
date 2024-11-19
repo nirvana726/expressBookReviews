c918428512d0420ccd9c64bdd593b457da31ce87
nirvana726 <vc77it18@vedascollege.edu.np>
2024-11-19 07:47:25 -0500

    Add Task 3: Get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,10 +32,17 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author; // Get author from request parameters
+    const filteredBooks = Object.values(books).filter((book) => book.author === author); // Filter books by author
+
+    if (filteredBooks.length > 0) {
+        res.send(filteredBooks); // Send the filtered books as a response
+    } else {
+        res.status(404).send("No books found by this author.");
+    }
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
