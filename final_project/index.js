671dae918f6acb4e2622edf14ad60764f14dd6c2
nirvana726 <vc77it18@vedascollege.edu.np>
2024-11-24 09:07:41 -0500

    index.js

--- final_project/index.js
+++ final_project/index.js
@@ -47,4 +47,4 @@ app.use("/", genl_routes);
 
 // Start the server
 const PORT = 5000;
-app.listen(PORT, () => console.log("Server is running on port", PORT));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
